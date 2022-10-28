18e8852204c55d4d1c6c6e7ad37e2976e59ac7d8
Jesus C. Compaire <108104717+jcompaire@users.noreply.github.com>
2022-10-28 13:54:05 -0300

    LICENSE

---
+++ LICENSE.R
@@ -0,0 +1,61 @@
+#   JCOMPAIRE_LICENSE License statement and permissions for
+#                     jcompaire/R repository.
+#   
+#   Copyright (C) 2022 J.C. Compaire
+#   
+#   ==========================================================================
+#   
+#   Citation
+#   
+#   This software is distributed free of charge.  If you use it in research
+#   resulting in a scientific presentation or publication, the software 
+#   should be acknowledged and cited as:
+#
+#   Compaire, J. C. (2022),  An R's toolbox to deal with big data processing
+#           and statistical analysis in marine sciences.
+#           https://github.com/jcompaire
+#   
+#   ==========================================================================
+#
+#   LICENSE
+#
+#   JCOMPAIRE is distributed under the 
+#   
+#     "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+#     (CC BY-NC-SA 4.0)"
+#   
+#   Version 4.0, available at 
+#
+#      https://creativecommons.org/licenses/by-nc-sa/4.0/
+#   
+#   You are free to:
+#   
+#       Share — copy and redistribute the material in any medium or format
+#   
+#       Adapt — remix, transform, and build upon the material
+#   
+#   Under the following terms:
+#   
+#       Attribution — You must give appropriate credit, provide a link to the
+#           license, and indicate if changes were made. You may do so in any
+#           reasonable manner, but not in any way that suggests the licensor
+#           endorses you or your use.
+#       
+#       NonCommercial — You may not use the material for commercial purposes.
+#   
+#       ShareAlike — If you remix, transform, or build upon the material,
+#           you must distribute your contributions under the same license
+#           as the original.
+#   
+#   ==========================================================================
+
+#   DISCLAMER
+#       
+#   The material embodied in this software is provided to you "as-is" and 
+#   without warranty of any kind, express, implied or otherwise, including
+#   without limitation, any warranty of fitness for a particular purpose.
+#   In no event will the author be held liable to anyone or any authority for
+#   any damages arising out of or in connection with the possession, use or
+#   performance of this software.
+#   
+#   ---
